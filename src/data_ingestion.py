63aa7a38c38a7bbc4539860d62ca88d26084e9f2
netra212 <netra200021kcbdr@gmail.com>
2025-07-08 15:55:17 +0100

    Data Ingestions

---
+++ src/data_ingestion.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+from google.cloud import storage
+from src.logger import get_logger
+from src.custom_exception import CustomException
+from config.paths_config import *
+from utils.common_functions import read_yaml
+
